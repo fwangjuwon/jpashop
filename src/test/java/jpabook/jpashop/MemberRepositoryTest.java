4519fb44fd6eba8528cc028a49eb9141824ee2a7
fwangjuwon <fwangjuwon@naver.com>
2023-01-31 23:36:36 +0900

    Test file 생성 - 미완성(@RunWith 어노테이션 임포트 에러)

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+}
